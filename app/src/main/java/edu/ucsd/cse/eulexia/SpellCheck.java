7c727a3a4ad1a58f024be915bbd54a042ce88138
scheah <sebcheah@gmail.com>
2015-10-30 17:56:46 -0700

    Fix file errors; Added alt dictionary

--- app/src/main/java/edu/ucsd/cse/eulexia/SpellCheck.java
+++ app/src/main/java/edu/ucsd/cse/eulexia/SpellCheck.java
@@ -8,37 +8,41 @@ import com.swabunga.spell.event.SpellChecker;
 import com.swabunga.spell.event.StringWordTokenizer;
 
 import java.io.File;
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+import java.util.HashMap;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.os.Environment;
 
 
 /**
  * Created by Sebastian on 10/26/2015.
  */
 public class SpellCheck implements SpellCheckListener {
-    private static String dictFile = "english.txt";
+    private static String dictFile = "english.0";
     private SpellChecker jazzySpellCheck = null;
     private String originalText;
+    public ArrayList<String> misspelledWords = new ArrayList<String>();
+    public HashMap<String, ArrayList<String> > suggestions = new HashMap<String, ArrayList<String>>();
 
     public SpellCheck(Context context, String bodyText) {
         originalText = bodyText;
-        AssetManager assetManager = context.getApplicationContext().getResources().getAssets();
+        AssetManager assetManager = context.getAssets();
         InputStream inputStream = null;
         SpellDictionary dictionary = null;
         try {
+            String filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/eulexia/english.txt";
             inputStream = assetManager.open(dictFile);
-            File file = createFileFromInputStream(inputStream);
+            File file = createFileFromInputStream(inputStream, filename);
             dictionary = new SpellDictionaryHashMap(file);
         } catch (IOException e) {
+            e.getMessage();
             e.printStackTrace();
         }
         jazzySpellCheck = new SpellChecker(dictionary);
@@ -47,11 +51,19 @@ public class SpellCheck implements SpellCheckListener {
     }
 
     public void spellingError(SpellCheckEvent event) {
-        List suggestions = event.getSuggestions();
+        List suggestionsList = event.getSuggestions();
+        String invalidWord = event.getInvalidWord();
         System.out.println("Invalid Word: " + event.getInvalidWord());
-        if (suggestions.size() > 0) {
-            for (Iterator suggestedWord = suggestions.iterator(); suggestedWord.hasNext();) {
-                System.out.println("\tSuggested Word: " + suggestedWord.next());
+        misspelledWords.add(event.getInvalidWord());
+        if (suggestionsList.size() > 0) {
+            for (Iterator suggestedWord = suggestionsList.iterator(); suggestedWord.hasNext();) {
+                String currentSuggestion = (String)suggestedWord.next();
+                ArrayList suggestionsForWord = suggestions.get(invalidWord);
+                if (suggestionsForWord != null)
+                    suggestionsForWord.add(currentSuggestion);
+                else
+                    suggestions.put(invalidWord, new ArrayList<String>());
+                System.out.println("\tSuggested Word: " + currentSuggestion);
             }
         }
         else {
@@ -60,22 +72,33 @@ public class SpellCheck implements SpellCheckListener {
     }
 
     // file helper function
-    private File createFileFromInputStream(InputStream inputStream) {
-        try{
-            File f = new File("copy.txt");
-            OutputStream outputStream = new FileOutputStream(f);
-            byte buffer[] = new byte[1024];
-            int length = 0;
-            while((length=inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer,0,length);
-            }
-            outputStream.close();
-            inputStream.close();
-            return f;
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    private File createFileFromInputStream(InputStream inputStream, String path) {
+        File directory = new File(Environment.getExternalStorageDirectory(), "eulexia");
+        directory.mkdirs();
+        File file = new File(Environment.getExternalStorageDirectory(), "eulexia/english.txt");
+        if (!file.exists()) {
+            try {
 
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+                byte buf[] = new byte[1024];
+                int len;
+                while ((len = inputStream.read(buf)) > 0) {
+                    fileOutputStream.write(buf, 0, len);
+                }
+
+                fileOutputStream.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file;
+    }
 }
