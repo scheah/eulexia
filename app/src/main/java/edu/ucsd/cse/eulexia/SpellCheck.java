b60ce04e95f81e04e48b1fbfcad973094ce7a32d
scheah <sebcheah@gmail.com>
2015-11-05 14:28:52 -0800

    Make singleton; remove file workaround;

--- app/src/main/java/edu/ucsd/cse/eulexia/SpellCheck.java
+++ app/src/main/java/edu/ucsd/cse/eulexia/SpellCheck.java
@@ -7,10 +7,10 @@ import com.swabunga.spell.event.SpellCheckListener; // asynchronous
 import com.swabunga.spell.event.SpellChecker;
 import com.swabunga.spell.event.StringWordTokenizer;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
@@ -18,87 +18,64 @@ import java.util.HashMap;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.os.Environment;
 
 
 /**
  * Created by Sebastian on 10/26/2015.
  */
 public class SpellCheck implements SpellCheckListener {
-    private static String dictFile = "english.0";
+    private static SpellCheck instance = null; // singleton, we do not want to keep re-reading a dictionary
+    private static String dictFile = "english.txt";
     private SpellChecker jazzySpellCheck = null;
     private String originalText;
     public ArrayList<String> misspelledWords = new ArrayList<String>();
     public HashMap<String, ArrayList<String> > suggestions = new HashMap<String, ArrayList<String>>();
 
-    public SpellCheck(Context context, String bodyText) {
-        originalText = bodyText;
+    protected SpellCheck(Context context/*, String bodyText*/) {
+        //originalText = bodyText;
         AssetManager assetManager = context.getAssets();
         InputStream inputStream = null;
         SpellDictionary dictionary = null;
         try {
-            String filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/eulexia/english.txt";
             inputStream = assetManager.open(dictFile);
-            File file = createFileFromInputStream(inputStream, filename);
-            dictionary = new SpellDictionaryHashMap(file);
+            Reader reader = new InputStreamReader(inputStream, "UTF-8");
+            dictionary = new SpellDictionaryHashMap(reader);
         } catch (IOException e) {
-            e.getMessage();
             e.printStackTrace();
         }
         jazzySpellCheck = new SpellChecker(dictionary);
-        jazzySpellCheck.addSpellCheckListener(this); // callbacks implemented in this class
-        jazzySpellCheck.checkSpelling(new StringWordTokenizer(bodyText)); // asynchronous
+        jazzySpellCheck.addSpellCheckListener(this); // callbacks implemented in this class, can possibly add listener to a different class
+        //jazzySpellCheck.checkSpelling(new StringWordTokenizer(bodyText)); // asynchronous
+    }
+
+    public static SpellCheck getInstance(Context context) {
+        if (instance == null) {
+            instance = new SpellCheck(context);
+        }
+        return instance;
+    }
+
+    public void checkWords(String bodyText) {
+        misspelledWords.clear();
+        suggestions.clear();
+        originalText = bodyText;
+        jazzySpellCheck.checkSpelling(new StringWordTokenizer(bodyText));
     }
 
     public void spellingError(SpellCheckEvent event) {
-        List suggestionsList = event.getSuggestions();
         String invalidWord = event.getInvalidWord();
-        System.out.println("Invalid Word: " + event.getInvalidWord());
+        List suggestionsList = event.getSuggestions();
+        //System.out.println("Invalid Word: " + event.getInvalidWord());
+        if (suggestions.containsKey(invalidWord)) // if we already recorded this misspell previously, exit
+            return;
         misspelledWords.add(event.getInvalidWord());
-        if (suggestionsList.size() > 0) {
-            for (Iterator suggestedWord = suggestionsList.iterator(); suggestedWord.hasNext();) {
-                String currentSuggestion = (String)suggestedWord.next();
-                ArrayList suggestionsForWord = suggestions.get(invalidWord);
-                if (suggestionsForWord != null)
-                    suggestionsForWord.add(currentSuggestion);
-                else
-                    suggestions.put(invalidWord, new ArrayList<String>());
-                System.out.println("\tSuggested Word: " + currentSuggestion);
-            }
+        ArrayList suggestionsForWord = new ArrayList<String>();
+        for (Iterator suggestedWord = suggestionsList.iterator(); suggestedWord.hasNext();) {
+            String currentSuggestion = (String) suggestedWord.next();
+            suggestionsForWord.add(currentSuggestion);
+            //System.out.println("\tSuggested Word: " + currentSuggestion);
         }
-        else {
-            System.out.println("\tNo suggestions");
-        }
-    }
+        suggestions.put(invalidWord, suggestionsForWord);
 
-    // file helper function
-    private File createFileFromInputStream(InputStream inputStream, String path) {
-        File directory = new File(Environment.getExternalStorageDirectory(), "eulexia");
-        directory.mkdirs();
-        File file = new File(Environment.getExternalStorageDirectory(), "eulexia/english.txt");
-        if (!file.exists()) {
-            try {
-
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream(file);
-
-                byte buf[] = new byte[1024];
-                int len;
-                while ((len = inputStream.read(buf)) > 0) {
-                    fileOutputStream.write(buf, 0, len);
-                }
-
-                fileOutputStream.close();
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return file;
     }
 }
