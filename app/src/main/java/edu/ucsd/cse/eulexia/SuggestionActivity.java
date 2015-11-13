7936d8ac599bf19b63c4035e227f14a9d2e9c1a4
Michelle Wu <maw035@ucsd.edu>
2015-11-12 23:13:33 -0800

    Added spelling out loud, swipe down gesture for spelling cards

--- app/src/main/java/edu/ucsd/cse/eulexia/SuggestionActivity.java
+++ app/src/main/java/edu/ucsd/cse/eulexia/SuggestionActivity.java
@@ -14,6 +14,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,19 +22,35 @@ import android.widget.AdapterView;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+import java.util.Locale;
+
 /**
  * Created by michelleawu on 11/12/15.
  * Creates a card scroll view containing all spelling suggestions.
  */
-public class SuggestionActivity extends Activity {
+public class SuggestionActivity extends Activity implements TextToSpeech.OnInitListener, GestureDetector.BaseListener {
 
     private static final String TAG = SpellCheckActivity.class.getSimpleName();
+
+    // Gesture detection
+    private GestureDetector mGestureDetector;
+
+    // Text to speech
+    private TextToSpeech tts;
+    private boolean initialized = false;
+    private String queuedText;
 
     // Index of misspelled words.
     // TODO: Create queue of misspelled words and cases for each. Currently, there is a hardcoded case example.
     // Visible for testing.
     static final int SUGG0 = 0;
     static final int SUGG1 = 1;
+    static final int SUGG2 = 2;
+    private String[] suggArray = {"tapioca", "typical", "topical"};
 
     private CardScrollAdapter mAdapter;
     private CardScrollView mCardScroller;
@@ -52,6 +69,11 @@ public class SuggestionActivity extends Activity {
         mCardScroller.setAdapter(mAdapter);
         setContentView(mCardScroller);
         setCardScrollerListener();
+
+        tts = new TextToSpeech(this /* context */, this /* listener */);
+
+        // Initialize the gesture detector and set the activity to listen to discrete gestures.
+        mGestureDetector = new GestureDetector(this).setBaseListener(this);
     }
 
     /**
@@ -60,16 +82,48 @@ public class SuggestionActivity extends Activity {
     private List<CardBuilder> createCards(Context context) {
         ArrayList<CardBuilder> cards = new ArrayList<CardBuilder>();
         cards.add(SUGG0, new CardBuilder(context, CardBuilder.Layout.MENU)
-                .setText("tapioca")
+                .setText(suggArray[SUGG0])
                 .setFootnote(R.string.suggestion_card_menu_description));
         cards.add(SUGG1, new CardBuilder(context, CardBuilder.Layout.MENU)
-                .setText("typical")
+                .setText(suggArray[SUGG1])
+                .setFootnote(R.string.suggestion_card_menu_description));
+        cards.add(SUGG2, new CardBuilder(context, CardBuilder.Layout.MENU)
+                .setText(suggArray[SUGG2])
                 .setFootnote(R.string.suggestion_card_menu_description));
         return cards;
 
      /*   Bundle params = getIntent().getExtras();
         String word = params.getString("word"); // TODO generate suggestions and display*/
 
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            initialized = true;
+            tts.setLanguage(Locale.ENGLISH);
+
+            if (queuedText != null) {
+                speak(queuedText);
+            }
+        }
+    }
+
+    public void speak(String text) {
+        // If not yet initialized, queue up the text.
+        if (!initialized) {
+            queuedText = text;
+            return;
+        }
+        queuedText = null;
+        // Before speaking the current text, stop any ongoing speech.
+        tts.stop();
+
+        // Set speech rate to a slower speed
+        tts.setSpeechRate(0.1f);
+
+        // Speak the text.
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
@@ -82,6 +136,10 @@ public class SuggestionActivity extends Activity {
     protected void onPause() {
         mCardScroller.deactivate();
         super.onPause();
+    }
+
+    private void spellWordAt(int index) {
+        speak(suggArray[index].replace("", " ").trim());
     }
 
     /**
@@ -98,10 +156,14 @@ public class SuggestionActivity extends Activity {
                 // On tap, play audio spelling
                 switch (position) {
                     case SUGG0:
-                        startActivity(new Intent(SuggestionActivity.this, SuggestionActivity.class));
+                        spellWordAt(SUGG0);
                         break;
                     case SUGG1:
-                        startActivity(new Intent(SuggestionActivity.this, SuggestionActivity.class));
+                        spellWordAt(SUGG1);
+                        //startActivity(new Intent(SuggestionActivity.this, SuggestionActivity.class));
+                        break;
+                    case SUGG2:
+                        spellWordAt(SUGG2);
                         break;
 
                     default:
@@ -116,28 +178,30 @@ public class SuggestionActivity extends Activity {
         });
     }
 
-    private GestureDetector createGestureDetector(Context context) {
-        GestureDetector gestureDetector = new GestureDetector(context);
-        //Create a base listener for generic gestures
-        gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
-            @Override
-            public boolean onGesture(Gesture gesture) {
-                Log.e("tag", gesture.name());
-                if (gesture == Gesture.TAP) {
-                    // play spelling out loud
-                    return true;
-                } else if (gesture == Gesture.SWIPE_RIGHT) {
-                    // go to next word
-                    return true;
-                } else if (gesture == Gesture.SWIPE_LEFT) {
-                    // go to prev word, or prev activity
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        return gestureDetector;
+    @Override
+    public boolean onGesture(Gesture gesture) {
+        Log.e("tag", gesture.name());
+        if (gesture == Gesture.TAP) {
+            return true;
+        } else if (gesture == Gesture.SWIPE_RIGHT) {
+            // go to next word
+            return true;
+        } else if (gesture == Gesture.SWIPE_LEFT) {
+            // go to prev word, or prev activity
+            return true;
+        }
+        else if (gesture == Gesture.SWIPE_DOWN) {
+            return false;
+        }
+        return false;
     }
 
+    /**
+     * Overridden to allow the gesture detector to process motion events that occur anywhere within
+     * the activity.
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        return mGestureDetector.onMotionEvent(event);
+    }
 }
--- app/src/main/java/edu/ucsd/cse/eulexia/audio/AudioSpelling.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsd.cse.eulexia.audio;
-
-/**
- * Created by michelleawu on 11/12/15.
- */
-public class AudioSpelling {
-}
